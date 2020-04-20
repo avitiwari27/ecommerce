c33f04bec19df405e1d44444eec38506f3460025
Avi tiwari <avithegr807@gmail.com>
2020-04-20 10:26:59 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import CheckoutForm from "./components/CheckoutForm/CheckoutForm";
+import items from "./api/api";
+import Product from "./components/Products/product";
+import Cart from "./components/Cart/Cart";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  const handleAddToCartClick = (id) => {
+    setItemsInCart((itemsInCart) => {
+      const itemInCart = itemsInCart.find((item) => item.id === id);
+
+      // if item is already in cart, update the quantity
+      if (itemInCart) {
+        return itemsInCart.map((item) => {
+          if (item.id !== id) return item;
+          return { ...item, quantity: item.quantity + 1 };
+        });
+      }
+
+      //otherwise, add new item to cart
+      const item = items.find((item) => item.id === id);
+      return [...itemsInCart, { ...item, quantity: 1 }];
+    });
+  };
+
+  const totalCost = itemsInCart.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-header-text">My Shop</h1>
       </header>
+      <main className="App-shop">
+        <div className="App-products">
+          {items.map((items) => (
+            <Product
+              key={items.title}
+              title={items.title}
+              price={items.price}
+              onAddToCartClick={() => handleAddToCartClick(items.id)}
+            />
+          ))}
+        </div>
+        <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
+        {itemsInCart.length > 0 && (
+          <StripeProvider apiKey="your_public_key">
+            <Elements>
+              <CheckoutForm totalCost={totalCost} />
+            </Elements>
+          </StripeProvider>
+        )}
+      </main>
     </div>
   );
 }
-
-export default App;
