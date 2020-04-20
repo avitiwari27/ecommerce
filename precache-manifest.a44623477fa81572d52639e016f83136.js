fc6fb0fc79cb65485b854a92b4e62e8de52b0881
Avi tiwari <avithegr807@gmail.com>
2020-04-20 11:05:43 +0545

    Updates

---
+++ precache-manifest.a44623477fa81572d52639e016f83136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b743ec2a0a200a80fad4ba9e025fd4ee",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "429e05a4242d40c0283b",
+    "url": "/ecommerce/static/css/main.a346d71d.chunk.css"
+  },
+  {
+    "revision": "cbd1eec9d931cac1a9ab",
+    "url": "/ecommerce/static/js/2.f2946846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce/static/js/2.f2946846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429e05a4242d40c0283b",
+    "url": "/ecommerce/static/js/main.fd275339.chunk.js"
+  },
+  {
+    "revision": "9d773cbe4ebd0b16874a",
+    "url": "/ecommerce/static/js/runtime-main.f7e9c32d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
